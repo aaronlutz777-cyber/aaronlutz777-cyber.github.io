512fad04fbc12b9bcb71cdea8710e13de67a7b48
aaronlutz777-cyber <aaronlutz777@gmail.com>
2026-03-24 09:14:03 -0300

    Add files via upload

---
+++ home.js
@@ -0,0 +1,119 @@
+// Reveals zoomed in versions of world-map using buttons and can reset too
+const zoomedImage = document.getElementById("zoom");
+const worldMapImage = document.getElementById("world-map");
+
+document.getElementById("reveal-vancouver-island").addEventListener('click', revealVancouverIsland);
+
+function revealVancouverIsland() {
+    zoomedImage.src = "Photos/vancouver-island-map.png";
+    zoomedImage.classList.remove("hidden");
+    zoomedImage.style.height = "60%"
+    worldMapImage.classList.add("hidden");
+}
+
+document.getElementById("reveal-europe").addEventListener('click', revealEurope);
+
+function revealEurope() {
+    zoomedImage.src = "Photos/europe-map.png";
+    zoomedImage.classList.remove("hidden");
+    zoomedImage.style.height = "90%"
+    worldMapImage.classList.add("hidden");
+}
+
+document.getElementById("reset-button").addEventListener('click', reset);
+
+function reset() {
+    zoomedImage.classList.add("hidden");
+    worldMapImage.classList.remove("hidden");
+}
+
+// allows locations links to highlight on home page
+let elements = document.getElementsByClassName("highlight");
+
+for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener("mouseover", mouseoverHandlerHighlight);
+    elements[i].addEventListener("mouseout", mouseoutHandlerHighlight);
+}
+
+function mouseoverHandlerHighlight(event) {
+    event.target.style.backgroundColor = "yellow";
+}
+
+function mouseoutHandlerHighlight(event) {
+    event.target.style.backgroundColor = "transparent";
+}
+
+// Notofication
+function displayNotification() {
+    const notification = document.createElement("div");
+
+    notification.innerText = "Hello fellow traveller!\nCome in, stay awhile... ";
+    notification.style.position = "fixed";
+    notification.style.top = "50%";
+    notification.style.left = "50%";
+    notification.style.transform = "translate(-50%, -50%)";
+    notification.style.border = "3px solid black";
+    notification.style.backgroundColor = "white";
+    notification.style.padding = "50px";
+
+    const closeButton = document.createElement("span");
+    closeButton.innerText = "|X|";
+    closeButton.style.position = "absolute";
+    closeButton.style.top = "10px";
+    closeButton.style.right = "10px";
+    closeButton.style.border = "2px solid black";
+    closeButton.style.cursor = "pointer";
+
+    closeButton.onclick = function() {
+        notification.remove(); 
+    }
+
+    notification.appendChild(closeButton);
+
+    document.body.appendChild(notification);
+}
+
+window.onload = function() {
+    if (!sessionStorage.getItem("notification-has-been-seen")) {
+        sessionStorage.setItem("notification-has-been-seen", "true");
+        displayNotification();
+    }
+};
+
+
+// Conditional
+const yes = document.getElementById("yes-button");
+const no = document.getElementById("no-button");
+const response = document.getElementById("response");
+let choice = "";
+
+if (!sessionStorage.getItem("response-given")) {
+
+    yes.addEventListener("click", function() {
+        sessionStorage.setItem("yesOrNo", "yes");
+        displayResponse("yes");
+    });
+
+    no.addEventListener("click", function() {
+        sessionStorage.setItem("yesOrNo", "no");
+        displayResponse("no");
+    });
+
+} else {
+    displayResponse(sessionStorage.getItem("yesOrNo"));
+}
+
+function displayResponse(choice) {
+    sessionStorage.setItem("response-given", "true");
+    
+    yes.classList.add("hidden");
+    no.classList.add("hidden");
+
+    if (choice === "yes") {
+        response.innerText = "Cool! Maybe you can tell me about your trip.";
+    } else if (choice === "no") {
+        response.innerText = "Maybe we could go there together someday.";
+    }
+}
+
+
---
+++ main.js
@@ -0,0 +1,94 @@
+
+// Event Handler
+let navElements = document.getElementsByClassName("italicize");
+
+for (let i = 0; i < navElements.length; i++) {
+    navElements[i].addEventListener("mouseover", mouseoverHandlerItalicize);
+    navElements[i].addEventListener("mouseout", mouseoutHandlerItalicize);
+}
+
+function mouseoverHandlerItalicize(event) {
+    event.target.style.fontStyle = "italic";
+    event.target.style.fontWeight = "bold";
+}
+
+function mouseoutHandlerItalicize(event) {
+    event.target.style.fontStyle = "normal";
+    event.target.style.fontWeight = "normal";
+}
+
+// Colour Change
+let headerColour = document.getElementsByClassName("header");
+let mainColour = document.getElementsByClassName("main");
+let navColour = document.getElementsByClassName("sidebar");
+
+const colourButton1 = document.getElementById("colour-scheme-1");
+const colourButton2 = document.getElementById("colour-scheme-2");
+const colourButton3 = document.getElementById("colour-scheme-3");
+
+colourButton1.addEventListener("click", changeToColourScheme1);
+colourButton2.addEventListener("click", changeToColourScheme2);
+colourButton3.addEventListener("click", changeToColourScheme3);
+
+function changeToColourScheme1() {
+    sessionStorage.setItem("colourScheme", "1");
+    colourButton1.style.backgroundColor = "yellow";
+    colourButton2.style.backgroundColor = "white";
+    colourButton3.style.backgroundColor = "white";
+
+    for (let i = 0; i < mainColour.length; i++) {
+        headerColour[i].style.backgroundColor = "rgb(11, 56, 11)";
+        headerColour[i].style.color = "rgb(165, 230, 13)";
+        mainColour[i].style.backgroundColor = "#78b1bf";
+        mainColour[i].style.color = "black";
+        navColour[i].style.backgroundColor = "blanchedalmond";
+    }
+}
+
+function changeToColourScheme2() {
+    sessionStorage.setItem("colourScheme", "2");
+    colourButton1.style.backgroundColor = "white";
+    colourButton2.style.backgroundColor = "yellow";
+    colourButton3.style.backgroundColor = "white";
+
+    for (let i = 0; i < mainColour.length; i++) {
+        headerColour[i].style.backgroundColor = "red";
+        headerColour[i].style.color = "yellow";
+        mainColour[i].style.backgroundColor = "orange";
+        mainColour[i].style.color = "blue";
+        navColour[i].style.backgroundColor = "pink";
+    }
+}
+
+function changeToColourScheme3() {
+    sessionStorage.setItem("colourScheme", "3");
+    colourButton1.style.backgroundColor = "white";
+    colourButton2.style.backgroundColor = "white";
+    colourButton3.style.backgroundColor = "yellow";
+
+    for (let i = 0; i < mainColour.length; i++) {
+        headerColour[i].style.backgroundColor = "grey";
+        headerColour[i].style.color = "black";
+        mainColour[i].style.backgroundColor = "white";
+        mainColour[i].style.color = "black";
+        navColour[i].style.backgroundColor = "black";
+        navColour[i].style.color = "white";
+    }
+}
+
+window.onload = function() {
+    if (!sessionStorage.getItem("notification-has-been-seen")) {
+        sessionStorage.setItem("notification-has-been-seen", "true");
+        changeToColourScheme1();
+    } else if (sessionStorage.getItem("colourScheme") == 1) {
+        changeToColourScheme1();
+    } else if (sessionStorage.getItem("colourScheme") == 2) {
+        changeToColourScheme2();
+    } else if (sessionStorage.getItem("colourScheme") == 3) {
+        changeToColourScheme3();
+    }
+};
+
+
+
+
